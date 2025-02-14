be3da50d32a6a74ac87f1808f32676d49d553ad6
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2025-02-14 18:02:41 -0500

    fix: correct syntax error in OUTPUT_DIR definition in workflow scripts

--- workflow/old_workflows/02-A_get_page_url_list.py
+++ workflow/old_workflows/02-A_get_page_url_list.py
@@ -20,7 +20,7 @@ from kiwifarmer import functions
 
 INPUT_DIR = '../../data_20210224/downloaded_threads/'
 
-OUTPUT_DIR =os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data')
+OUTPUT_DIR =os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data'))
 
 PAGE_LIST_FILENAME = 'page_url_list.txt'
 
